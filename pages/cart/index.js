14b6174757c6353ff0cf9ef693185180e2720c88
rezenek <anasmslehereif@gmail.com>
2021-01-27 23:18:55 +0300

    latest

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import img from "../../public/images/product-img-1.jpg";
 import {useEffect, useState} from 'react';
 import Footer from '../../components/common/footer/footer';
+import close from '../../public/images/icons8-macos-close-30.png';
 export default function Cart(props) {
     const [noCart, setNocart] = useState(false);
     const [cartObj, setCartObj] = useState([]);
@@ -11,13 +12,29 @@ export default function Cart(props) {
         if(localStorage.cartArr){
             setCartObj(cartObj.concat(JSON.parse(localStorage.cartArr)));
         }
-        console.log(cartObj)
+
         if(localStorage.cartArr){
            null
         }else{
             setNocart(true)
         }
     },[])
+    const removeItem = function (id){
+        console.log(id)
+        var cartObj = JSON.parse(localStorage.cartArr);
+        for (let i=0; i < cartObj.length; i++){
+            if (cartObj[i]['id'] == Number(id)){
+                cartObj.splice(i, 1);
+                if(cartObj.length == 0){
+                    setNocart(true)
+                }
+                localStorage.cartArr = JSON.stringify(cartObj);
+                document.getElementById(id).remove()
+            }
+        }
+        var cartNumber = document.getElementsByClassName("cart_num")[0];
+        cartNumber.innerHTML =  Number(JSON.parse(localStorage.cartArr).length)
+    }
         return(
             <div className={'cart'}>
                 <div className={'pr_title flex container'}>
@@ -34,18 +51,22 @@ export default function Cart(props) {
                     </div>:null
                 }
                 <div className={'products flex_row_btw container'}>
-                {cartObj.map(pr =>{
+                {cartObj.map((pr,index) =>{
                     return(
-                            <div className={'product'} id={pr.id}>
+                            <div className={'product'} id={pr.id} key={index}>
                                 <div className={'imgCont flex'}>
                                     <Link href={'/product/'+pr.id}>
                                         <img src={pr.image} />
                                     </Link>
+                                    <span className="removeItem" title="remove this item" id={pr.id} onClick={removeItem.bind(this,pr.id)}>
+                                        <img src={close} />
+                                    </span>
                                 </div>
+
                                 <Link href={"/product"+pr.id}>
-                            <span className={"productName"}>
-                                {pr.title}
-                                 </span>
+                                    <span className={"productName"}>
+                                        {pr.title}
+                                        </span>
                                     </Link>
                                     <span className={"price"}>
                                     {pr.price}$
@@ -87,6 +108,11 @@ export default function Cart(props) {
                           cursor: pointer;
                           align-items: center;
                           justify-content: center;
+                          .removeItem{
+                          position: absolute;
+                          top: 10px;
+                          right: 10px;
+                          }
 
                           img {
                             width: 100%;
